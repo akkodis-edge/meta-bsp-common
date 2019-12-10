185217890d955160de030cb6dabf2c2e8ac86d28
Mikko Salomäki <ms@datarespons.se>
2019-12-10 15:43:20 +0100

    login-autorun: bump 1.1, allow aborting by pressing any key

--- recipes-system/login-autorun/files/99-autorun.sh
+++ recipes-system/login-autorun/files/99-autorun.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+TIMEOUT=3
+
 echo ""
 if [ -x /usr/bin/autorun ]; then
-	/usr/bin/autorun
+	if read -r -s -n 1 -t ${TIMEOUT} -p "Press any key to abort autorun in the next ${TIMEOUT} seconds... " key; then
+		echo "aborted"
+	else
+		/usr/bin/autorun
+	fi
 else
 	echo "/usr/bin/autorun: not available"
 fi
